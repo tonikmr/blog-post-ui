12ff9454cf88f58c4bbe94a92e21601356f69fd7
Rohit Kumar <rohitsmcet@gmail.com>
2023-08-15 18:37:23 +0530

    add

--- script/login.js
+++ script/login.js
@@ -19,7 +19,7 @@ $("#login-form").on("submit", function (event) {
         localStorage.setItem("user", JSON.stringify(data.user));
         alert("Log in successfully.");
         setTimeout(() => {
-          location.href = "/UI/html/home.html";
+          location.href = "/UI/html/index.html";
         }, 200);
       }
       if (data && !data.isExist) {
